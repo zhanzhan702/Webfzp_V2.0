904a75296073abf47b32423b5d5e3bf1f808a4d8
zhanzhan <2837420590@qq.com>
2026-05-08 14:51:31 +0800

    重构时间显示和框架高度调整功能，移除冗余代码

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-Date.prototype.format = function (format) {
+       Date.prototype.format = function (format) {
            var args = {
                "M+": this.getMonth() + 1,
                "d+": this.getDate(),
@@ -17,3 +17,42 @@ Date.prototype.format = function (format) {
            }
            return format;
        };
+       function isMobileDevice() {
+            var device = navigator.userAgent ;          
+            return (device.indexOf("iPhone")>0||device.indexOf("Android")>0);
+        }
+
+        function updateTime() {
+            var now = new Date();
+            var text = isMobileDevice()
+                ? now.format("hh:mm:ss")
+                : now.format("yyyy-MM-dd hh:mm:ss");
+            document.getElementById("timeDisplay").textContent = text;
+        }
+
+        function adjustFrameHeight() {
+            var header = document.querySelector(".header-wrapper");
+            var footer = document.querySelector(".footer-section");
+            var mainFrame = document.querySelector(".mainFrame");
+            var height = document.documentElement.clientHeight
+                - (header ? header.clientHeight : 0)
+                - (footer ? footer.clientHeight : 0)
+                -24;
+                // 补足footer的padding部分
+            if (height < 0) height = 0;
+            mainFrame.style.height = height + "px";
+        }
+
+        window.addEventListener("load", function () {
+            updateTime();
+            adjustFrameHeight();
+            setInterval(function () {
+                updateTime();
+                adjustFrameHeight();
+            }, 1000);
+        });
+
+        window.addEventListener("resize", function () {
+            adjustFrameHeight();
+            updateTime();
+        });
